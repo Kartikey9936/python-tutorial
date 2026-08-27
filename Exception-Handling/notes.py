5b00a2f18762a869cb459f293b07ac45aad18871
Kartikey <kartikeykesharwani9936@gmail.com>
2026-08-27 20:12:26 +0530

    exception handling added

---
+++ Exception-Handling/notes.py
@@ -0,0 +1,35 @@
+#syntax error happens during compilation of the code. It is a mistake in the syntax of the code. 
+#exception during execution of the code.(run time error) It is a mistake in the logic of the code.
+
+#indexError: This error occurs when trying to access an index that is out of range 
+#indexerrror shown in list,tupple,string
+
+
+#moduleNotFoundError: This error occurs when trying to import a module that does not exist
+
+
+#keyError: This error occurs when trying to access a key that does not exist in a dictionary
+
+
+#valueError: This error occurs when a function receives an argument of an inappropriate type
+#int('a)
+
+#typeError: This error occurs when an operation or function is applied to an object of inappropriate type
+
+1 + 'a'                                                                                                                                             
+
+#exception
+# overflowError: This error occurs when a calculation exceeds the maximum limit for a numeric type
+#logicError: devide by zero
+#databaseError: This error occurs when there is an issue with the database operation, such as a connection failure or a query error
+
+# WHY WE NEED EXCEPTION HANDLING?
+# 1. USER FRIENDLY ERROR MESSAGE INSTEAD OF STACKTRACE
+# LIKE USING A PAYMENT GATEWAY AND THE PAYMENT FAILS, IT SHOULD SHOW A USER FRIENDLY ERROR MESSAGE INSTEAD OF STACKTRACE
+
+# 2. TO SECURE OUR CODE INFORMATION FROM HACKER
+
+
+
+# TRY EXCEPT BLOCK - TO HANDLE EXCEPTIONS
+
---
+++ py-point.py
@@ -0,0 +1,28 @@
+# Dictionary
+
+# {
+#  name : Alice
+#  age  : 21
+#  city : Delhi
+# }
+
+#         │
+#         │ **
+#         ▼
+
+# student(
+#     name="Alice",
+#     age=21,
+#     city="Delhi"
+# )
+
+nums = [10,20,30]
+print(*nums)
+
+dict = {
+    "name": "kartikey",
+    "age" : 20
+
+}
+student(**dict)
+
